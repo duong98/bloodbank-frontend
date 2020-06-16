6d221c195684dcef5bf0015b645bf119d204c308
duong98 <20165933#student.hust.edu.vn>
2020-06-17 06:51:16 +0700

    test server

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 const withImages = require("next-images");
 module.exports = withImages({
   env: {
-    host: "http://35.223.238.198:8080/api",
+    host: "https://35.223.238.198:8080/api",
   },
 });
